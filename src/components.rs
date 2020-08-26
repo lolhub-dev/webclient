589bc11e890caa995e3678a5518539919e3a529d
Daniel Gusenburger <danielgusenburger@online.de>
2020-08-26 18:37:18 +0200

    added driver, port, gateway folders

--- src/components.rs
+++ src/components.rs
@@ -2,12 +2,12 @@ use seed::{prelude::*, *};
 
 pub fn view_hero<Ms>() -> Node<Ms> {
     div![
-        C!["hero", "is-medium", "is-primary", "is-bold", "has-bg-img"],
+        C!["hero", "is-medium", "is-light", "is-bold", "has-bg-img"],
         div![
             C!["hero-body"],
             div![
                 C!["container", "has-text-centered"],
-                h1![C!["title"], "lol:Hub"],
+                h1![C!["title", "is-size-1"], "lol:Hub"],
             ],
         ],
     ]
