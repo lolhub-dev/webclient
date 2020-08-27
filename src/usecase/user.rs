12121657320b9cf0a5f627f386bd9a9878322f75
Daniel Gusenburger <danielgusenburger@online.de>
2020-08-27 20:56:24 +0200

    changed &UserPort to &impl UserPort

--- src/usecase/user.rs
+++ src/usecase/user.rs
@@ -1,6 +1,6 @@
 use crate::domain::user;
 use crate::port::user_port::{AuthResult, UserPort};
 
-pub fn login_user(gateway: &UserPort, credentials: &user::Credentials) -> AuthResult<user::User> {
+pub fn login_user(gateway: &impl UserPort, credentials: &user::Credentials) -> AuthResult<user::User> {
     gateway.login(credentials)
 }
